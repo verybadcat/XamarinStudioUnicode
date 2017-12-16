4ebd0f4c77ee4294056c58fd5c0b3fa1c158699f
William Jockusch <jockusch@gmail.com>
2017-12-16 09:13:08 -0600

    couple Unicode tests

---
+++ XamarinStudioUnicode/UnicodeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace XamarinStudioUnicode
+{
+    [TestFixture]
+    public class UnicodeTests
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+
+        [Test]
+        public void TestUtf32()
+        {
+            var input = 0x0001D45A; // mathematical italic small m
+
+            var stringified = Char.ConvertFromUtf32(input);
+            var chars = stringified.ToCharArray();
+            var char0 = chars[0];
+            var char1 = chars[1];
+
+            
+        }
+    }
+}
